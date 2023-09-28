0f4ea2b86ca1118b20e726236191ddc9c246e737
yash244466666 <yash.solo.000@gmail.com>
2023-09-28 10:41:34 +0600

    Add CRUD for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,68 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
 
     def show
+        # @article = Article.find(params[:id])
+    end
+
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+
+    end
+
+    def edit
+        # @article = Article.find(params[:id])
+    end
+
+    def create
+        @article = Article.new(article_params)
+        # if
+        #     @article.save
+        #     redirect_to articles_path(@article)
+        # else
+        #     flash.now[:error] = @article.errors.full_messages
+        # end
+        respond_to do |format|
+            if @article.save
+              format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
+            #   format.json { render :show, status: :created, location: @article }
+            else
+              format.html { render :new, status: :unprocessable_entity }
+            #   format.json { render json: @article.errors, status: :unprocessable_entity }
+            end
+          end
+    end
+
+    def update
+        # @article = Article.find(params[:id])
+        respond_to do |format|
+            if @article.update(article_params)
+                format.html { redirect_to article_url(@article), notice: "Article was successfully updated ." }
+            else
+                format.html { render :edit, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    def destroy
+        # @article = Article.find(params[:id])
+        @article.destroy
+        respond_to do |format|
+            format.html { redirect_to articles_url(@article), notice: "Article was successfully destroyed." }
+        end
+    end
+
+    private
+
+    def set_article
         @article = Article.find(params[:id])
     end
 
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
   get "/about", to: "home#about"
-  resources :articles, only: [:show]
+  resources :articles #, only: [:show, :index, :new, :create, :edit, :update, :destroy]
 end
